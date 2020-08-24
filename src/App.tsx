3c8c8f475f11dfb2781306fd3561eedff2ed7519
Thejus Paul <thejuspaul@pm.me>
2020-08-24 12:11:14 -0700

    Minor UX Improvements
    feat:
    1. add close button for the modal box
    2. add autofocus on input box when modal box is opened
    refactor:
    1. seperate `insertTask` function from `closeModalBox`
    style:
    1. increase height of modal box to accomodate close button
    2. add styling for close button

--- src/App.tsx
+++ src/App.tsx
@@ -22,18 +22,25 @@ class App extends Component<{}, State> {
     
     this.openModalBox = this.openModalBox.bind(this)
     this.closeModalBox = this.closeModalBox.bind(this)
+    this.insertTask = this.insertTask.bind(this)
     this.getTaskName = this.getTaskName.bind(this)
     this.onTaskDelete = this.onTaskDelete.bind(this)
   }
   openModalBox = () => {
     const ADD_TASK = document.getElementById("add_task")
     const OVERLAY = document.getElementById("overlay")
+    const TASK_NAME = document.getElementById("task_name")
     if(ADD_TASK) ADD_TASK.classList.toggle("closed")
     if(OVERLAY) OVERLAY.classList.toggle("closed")
+    if(TASK_NAME) TASK_NAME.focus()
   }
   closeModalBox = () => {
     const ADD_TASK = document.getElementById("add_task")
     const OVERLAY = document.getElementById("overlay")
+    if(ADD_TASK) ADD_TASK.classList.toggle("closed")
+    if(OVERLAY) OVERLAY.classList.toggle("closed")
+  }
+  insertTask = () => {
     if(this.state.taskName !== "") {
       let task: object = {
         id: this.state.taskID,
@@ -48,8 +55,7 @@ class App extends Component<{}, State> {
         todoList: currentTodoList
       });
     }
-    if(ADD_TASK) ADD_TASK.classList.toggle("closed")
-    if(OVERLAY) OVERLAY.classList.toggle("closed")
+    this.closeModalBox()
   }
   getTaskName = (e:{target: HTMLInputElement}) => {
     this.setState({taskName: e.target.value}) 
@@ -73,11 +79,12 @@ class App extends Component<{}, State> {
           <ButtonList openModalBox={this.openModalBox} />
         </main>
         <section className="add_task closed" id="add_task">
+          <span id="close" onClick={this.closeModalBox}>&#xd7;</span><br/>
           <div className="wrapper">
             <label>Name:</label>
             <input type="text" id="task_name" onBlur={this.getTaskName}/> 
           </div>
-          <input className="btn btn__add" onClick={this.closeModalBox} type="submit" value="OK" />
+          <input className="btn btn__add" onClick={this.insertTask} type="submit" value="OK" />
         </section>
         <section className="overlay closed" id="overlay"></section>
       </div>
